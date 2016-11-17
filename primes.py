fdff9f69a82ef84797f9872e9784b234766f4d30
Armando Perez <armandoyo@gmail.com>
2016-11-16 23:04:30 -0400

    py1
    Prueba python 1

--- primes.py
+++ primes.py
@@ -1,16 +1,28 @@
 """
-Your module documentation here
+Modulo para implementar si un numero es primo
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    clase que verifica si un numero es primo
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        metodo que implementa si un numero es primo
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        self.numero = num_int
+        if self.numero == 1 or self.numero == 2:  # si el numero 1 o 2 es primo
+            return True
+        else:  # sino se verifica que solo se divida por 1 y el
+            i = 2
+            while i < self.numero:
+                numprimo = self.numero % i
+                if numprimo == 0:
+                    i = self.numero
+                i = i + 1
+            if numprimo == 0:
+                return False
+            else:
+                return True
