96a3ceaec1413062e212684c131766b07b862092
Armando Perez <armandoyo@gmail.com>
2016-11-16 23:35:06 -0400

    Update primes.py

--- primes.py
+++ primes.py
@@ -8,19 +8,18 @@ class PrimeClass(object):
     clase que verifica si un numero es primo
     """
 
-    def is_prime(self, num_int):
+    def is_prime(self, numero):
         """
         metodo que implementa si un numero es primo
         """
-        self.numero = num_int
-        if self.numero == 1 or self.numero == 2:  # si el numero 1 o 2 es primo
+        if numero == 1 or numero == 2:  # si el numero 1 o 2 es primo
             return True
         else:  # sino se verifica que solo se divida por 1 y el
             i = 2
-            while i < self.numero:
-                numprimo = self.numero % i
+            while i < numero:
+                numprimo = numero % i
                 if numprimo == 0:
-                    i = self.numero
+                    i = numero
                 i = i + 1
             if numprimo == 0:
                 return False
